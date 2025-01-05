4a6d7debec926edb2ed8f50ee194e0701d789812
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-01-05 23:12:26 +0100

    add hotreus62 keymap

---
+++ keyboards/atreus62/keymaps/cn/rules.mk
@@ -0,0 +1,19 @@
+# VIA_ENABLE = yes
+# VIAL_ENABLE = yes
+# VIAL_INSECURE = yes
+LTO_ENABLE = yes
+# KEY_OVERRIDE_ENABLE = no
+# QMK_SETTINGS = no
+
+# Bootloader selection
+# BOOTLOADER = rp2040
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# RAW_ENABLE = yes
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+IOS_DEVICE_ENABLE       = yes
+UNICODE_ENABLE = yes
